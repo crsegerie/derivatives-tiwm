e3c72825ce71c7ca897dc8d627bef07d3f8dd367
crsegerie <charbel-raphael.segerie@hotmail.fr>
2021-06-02 00:11:11 +0200

    initial commit

---
+++ config.py
@@ -0,0 +1,84 @@
+# General
+study_name = 'time_in_wm'
+# bids_root = '/home/charb/Desktop/parietal/time_in_wm_new/'
+bids_root = '/storage/store2/data/time_in_wm_new'
+task = 'tiwm'
+interactive = False
+
+# subjects = ["155", "197", "215", "220"]  # etc..
+subjects = ["155"]
+exclude_subjects = ["586", "812"]
+# 586 only 6 runs. 812: 3 runs.
+# warning : subjects with 9 runs: 215, 231, 653 : one of the run was aborted.
+
+# runs = "all"  # Bug > result in 9 imported and crash
+# runs = ["01", "02", "03", "04", "05", "06", "07", "08"]
+runs = ["01"]
+
+find_flat_channels_meg = False
+find_noisy_channels_meg = False
+ch_types = ['meg']
+
+# Max filter
+use_maxwell_filter = True
+mf_reference_run = '01'  # To do change it to run 4
+mf_head_origin = 'auto'  # 'auto', it will be estimated from headshape points.
+
+# If you are interested in low frequency activity (<0.1Hz), avoid using tSSS
+# and set mf_st_duration to None.
+mf_st_duration = None
+
+# Filtering and resampling
+l_freq = 1.
+h_freq = 150.
+resample_sfreq = 500.
+decim = 1  # no decimation
+
+# Artifact correction.
+spatial_filter = 'ica'
+ica_max_iterations = 500
+ica_l_freq = 1.
+ica_decim = 4
+ica_n_components = 0.999
+ica_reject_components = 'auto'
+ica_reject = {'mag': 5e-11}  # reject_beforeICA = {'mag': 5e-11}
+reject_afterICA = {'grad': 4000e-13, 'mag': 1e-11}  # reject but not found
+rejcomps_man = dict()  # Not understood if necesssary here
+ica_ctps_ecg_threshold = 0.1  # Default value not present in documentation.
+ica_eog_threshold = 3.0
+
+# Epochs
+epochs_tmin = -4.6
+epochs_tmax = 5
+trigger_time_shift = 0  # Not understood and not found in the doc
+baseline = (-1, 0)
+
+# Noise estimation
+process_er = True
+noise_cov = 'emptyroom'
+
+
+# event_id : not needed because already in events.tsv
+conditions = ["1_item/short", "1_item/medium", "1_item/long",
+              '3_item/short', '3_item/medium', '3_item/long', ]
+
+
+# Decoding
+decoding_metric = 'roc_auc'
+decoding_n_splits = 8
+
+# Time frequency
+time_frequency_conditions = conditions
+
+# Source space parameters
+spacing = 'oct6'
+mindist = 5
+# loose = 0.2  # set as "auto" but auto = 0.2
+# depth = 0.8  # not in the pipeline but in mne as 0.8. So ok
+inverse_method = 'dSPM'
+# smooth = 10  # not found ok
+
+
+# Advanced parameters
+l_trans_bandwidth = 'auto'
+h_trans_bandwidth = 'auto'
